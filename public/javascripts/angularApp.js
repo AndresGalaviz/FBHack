5c4b04aad1ed1e75b89dd47ed8b812841fc0e565
patrickwijerama <patrick.wijerama@gmail.com>
2014-10-25 02:34:25 -0500

    Now front end and back end can communicate

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -5,6 +5,19 @@ angular.module('feedMe', ['ui.router'])
   };
   return o;
 }])
+.factory('restaurants', ['$http', function($http){
+  var o = {
+  	restaurants: []
+  };
+
+	o.getAll = function() {
+    return $http.get('/restaurants').success(function(data){
+      angular.copy(data, o.restaurants);
+    });
+  };
+
+  return o;
+}])
 .controller('MainCtrl', [
 	'$scope',
 	'posts', 
@@ -39,6 +52,41 @@ angular.module('feedMe', ['ui.router'])
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes +=1;
 		}
+}]).controller('RestaurantsCtrl', [
+	'$scope',
+	'restaurants', 
+	'restaurantPromise', 
+	function($scope, restaurants, restaurantPromise){
+		$scope.restaurants = restaurants.restaurants;
+		$scope.test  = "Hello world!";
+
+		$scope.addRestaurant = function () {
+			console.log($scope.name);
+			if(!$scope.name || $scope.name === '') {return;}
+			$scope.restaurants.push({
+				name: $scope.name,
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+				    {author: 'Joe', body: 'Cool post!' + $scope.name, upvotes: 0},
+				    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				  ]
+			});
+			$scope.name='';
+			$scope.link='';
+		};
+		$scope.addComment = function(){
+		  if(!$scope.body || $scope.body === '') { return; }
+		  $scope.restaurants.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+		$scope.incrementUpvotes = function(restaurants) {
+			restaurants.upvotes +=1;
+		}
 }]).controller('PostsCtrl', [
 	'$scope',
 	'$stateParams',
@@ -55,10 +103,24 @@ angular.module('feedMe', ['ui.router'])
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
 			})
+			.state('restaurants', {
+				resolve: {
+ 					restaurantPromise: ['restaurants', function(restaurants){
+    					return restaurants.getAll();
+  					}]
+  				},
+				url: '/restaurants',
+				templateUrl: '/restaurants.html',
+				controller: 'RestaurantsCtrl'
+			}).state('restaurant', {
+				url: '/restaurants/{id}',
+				templateUrl: '/restaurant.html',
+				controller: 'RestaurantCtrl'
+			})
 			.state('posts', {
 				url:'/posts/{id}',
 				templateUrl: '/posts.html',
 				controller: 'PostsCtrl'
 			});
-	$urlRouterProvider.otherwise('home');
+	$urlRouterProvider.otherwise('restaurants');
 }])

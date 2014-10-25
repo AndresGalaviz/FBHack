f1284af94c75acd1dfa39cea753dccfd79072fca
patrickwijerama <patrick.wijerama@gmail.com>
2014-10-25 00:23:27 -0500

    Restaurants routes are now possible

---
+++ models/Restaurants.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var RestaurantSchema = new mongoose.Schema({
+	name: String,
+	link: String,
+	upvotes: {type: Number, default: 0},
+	comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+});
+
+RestaurantSchema.methods.upvote = function(cb) {
+ 	this.upvotes += 1;
+ 	this.sabe(cb);
+ }
+
+mongoose.model('Restaurant', RestaurantSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,153 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var Restaurant = mongoose.model('Restaurant');
+
+
+//POst functions:
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+  console.log(req.body + ', '+ req.body.title)
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
+
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    res.json(post);
+  });
+});
+
+
+
+
+router.get('/init', function(req, res, next) {
+  Restaurant.remove({}, function(err) { 
+  	 console.log('collection removed') 
+  });
+  var restaurant;
+  for(var i=0; i<10; i++){
+  	restaurant = new Restaurant({name:'restaurant'+i})
+  	restaurant.save()
+  }
+  res.json(restaurant);
+
+});
+
+
+
+router.get('/restaurants', function(req, res, next) {
+  Restaurant.find(function(err, restaurants){
+    if(err){ return next(err); }
+
+    res.json(restaurants);
+  });
+});
+
+
+router.post('/reastaurants', function(req, res, next) {
+  var restaurant = new Restaurant(req.body);
+  console.log(req.body + ', '+ req.body.title)
+  restaurant.save(function(err, restaurant){
+    if(err){ return next(err); }
+
+    res.json(restaurant);
+  });
+});
+
+
+router.param('restaurant', function(req, res, next, id) {
+  var query = Restaurants.findById(id);
+
+  query.exec(function (err, restaurant){
+    if (err) { return next(err); }
+    if (!restaurant) { return next(new Error("can't find restaurant")); }
+
+    req.restaurant = restaurant;
+    return next();
+  });
+});
+
+
+router.get('/restaurants/:restaurant', function(req, res) {
+  res.json(req.restaurant);
+});
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+
+
 
 module.exports = router;
